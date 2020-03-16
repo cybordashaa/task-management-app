a3c8c8b73e570fc29189261e6b6138acda319dc4
dashaa <cybordashaa@gmail.com>
2020-03-16 19:29:07 +0800

    add: user sign up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import {UserRepository} from "./user.repository";
+import {InjectRepository} from "@nestjs/typeorm";
+import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
+    }
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
+        return this.userRepository.signUp(authCredentialsDto);
+    }
+
+
+}
