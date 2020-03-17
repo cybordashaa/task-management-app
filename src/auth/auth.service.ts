aa3c1d911a67c48c062b38a17458650ff9bf94f7
dashaa <cybordashaa@gmail.com>
2020-03-17 12:51:55 +0800

    add: jwt token upon authencation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,23 +2,29 @@ import {Body, Injectable, UnauthorizedException, ValidationPipe} from '@nestjs/c
 import {UserRepository} from "./user.repository";
 import {InjectRepository} from "@nestjs/typeorm";
 import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
+import {JwtService} from "@nestjs/jwt";
+import {JwtPayload} from "./jwt-payload.interface";
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
+    constructor(@InjectRepository(UserRepository) private userRepository: UserRepository, private jwtService: JwtService) {
     }
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
         return this.userRepository.signUp(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto){
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string}>{
         const username = await  this.userRepository.validateUserPassword(authCredentialsDto);
         //console.log(result);
 
         if(!username){
             throw new UnauthorizedException('Invalid crendentials');
         }
+
+        const payload: JwtPayload = { username };
+        const accessToken = await this.jwtService.sign(payload);
+        return { accessToken };
     }
 }
