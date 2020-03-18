0d787f32972dc44f1b969974beb7fbfe99482d12
dashaa <cybordashaa@gmail.com>
2020-03-18 15:59:12 +0800

    add: Authorization getting users task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -35,8 +35,11 @@ export class TasksController {
   }
 
   @Get('/:id')
-  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-    return this.tasksService.getTaskById(id);
+  getTaskById(
+      @Param('id', ParseIntPipe) id: number,
+      @GetUser() user: User
+  ): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
   }
 
   @Post()
@@ -50,11 +53,11 @@ export class TasksController {
     return this.tasksService.deleteTask(id);
   }
 
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id', ParseIntPipe) id: number,
-    @Body('status', TaskStatusValidationPipe) status: TasksStatus,
-  ): Promise<Task> {
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @Body('status', TaskStatusValidationPipe) status: TasksStatus,
+  // ): Promise<Task> {
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,8 +19,11 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(
+      id: number,
+      user: User
+      ): Promise<Task> {
+    const found = await this.taskRepository.findOne({ where: { id, userId: user.id}});
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -38,10 +41,10 @@ export class TasksService {
     }
     console.log(result);
   }
-  async updateTaskStatus(id: number, status: TasksStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    await task.save();
-    return task;
-  }
+  // async updateTaskStatus(id: number, status: TasksStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   await task.save();
+  //   return task;
+  // }
 }
