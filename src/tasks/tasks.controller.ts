9269c9fbf7006f34c859fa863e30f9a6489d6db3
dashaa <cybordashaa@gmail.com>
2020-03-19 11:47:30 +0800

    add: authorization deleting a users task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -48,8 +48,8 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: number): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  deleteTask(@Param('id', ParseIntPipe) id: number, @GetUser() user: User): Promise<void> {
+    return this.tasksService.deleteTask(id,user);
   }
 
   @Patch('/:id/status')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -33,8 +33,11 @@ export class TasksService {
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     return this.taskRepository.createTask(createTaskDto, user);
   }
-  async deleteTask(id: number): Promise<void> {
-    const result = await this.taskRepository.delete(id);
+  async deleteTask(
+      id: number,
+      user: User,
+      ): Promise<void> {
+    const result = await this.taskRepository.delete( id,);
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "" not found`);
     }
